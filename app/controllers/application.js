d7f31d7ef4545fb9b1f14160039be95ca19fe107
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-03-24 21:47:47 +0100

    adding polylines

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
   zoom: 17,
   centerLat: 14.7646531,
-  centerLng: 102.8115874
+  centerLng: 102.8115874,
+  
+  polyline: Ember.computed('model', function() {
+    return {
+	  path: this.get('model'), 
+	  isEditable: true
+	};
+  }),
+  
+  polylinesArray: Ember.computed('polyline', function() {
+    return [ this.get('polyline') ];
+  })
 });
