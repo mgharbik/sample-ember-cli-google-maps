0d7191c0cc01060ea8ea080497b5d5d7d646e24b
Gharbi Mohammed <simo163@hotmail.co.uk>
2015-03-24 21:36:51 +0100

    adding markers to the map

---
+++ app/routes/application.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	model: function() {
+		return Ember.A([
+	    	{title: "Home", lat: 14.766127, lng: 102.810987, body: "Here is my home"},
+	      	{title: "Shop", lat: 14.762963, lng: 102.812285, body: "Here is my shop", isInfoWindowVisible: true},
+	      	{title: "Job", lat: 14.762900, lng: 102.812018, body: "Here is my job"}
+	   ]);
+	}
+});
